9b11ec548a126dc32877fdf33600ab255031884f
KIkiBotF <d032410361@student.utem.edu.my>
2026-06-29 01:23:30 +0800

    mobile interface

--- sidebar.php
+++ sidebar.php
@@ -27,8 +27,8 @@
     cursor: pointer;
     text-align: center;
     gap: 5px;
-    text-decoration: none; 
-    color: #000; 
+    text-decoration: none;
+    color: #000;
 }
 
 .nav-item span {
@@ -54,61 +54,81 @@
     margin-top: -10px;
 
 }
-@media (max-width: 780px){
-    body{
+@media (max-width: 780px) {
+    body {
         display: block !important;
-        padding-bottom: 80px;
+        padding-bottom: 80px; 
     }
-    .sidebar{
+    
+    .sidebar {
         position: fixed;
         bottom: 0;
         left: 0;
         width: 100%;
-        height: 70px;
+        height: 75px; /* Increased slightly to give the larger icons room */
         flex-direction: row;
         justify-content: space-around;
+        align-items: center; /* Ensures items don't stretch */
         padding-top: 0;
-        box-shadow: 0 -2px 10px rgba(0, 0, 0,0.1);
+        box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.1);
         z-index: 1000;
     }
-    .nav-item{
-        padding: 10px;
-    }
-    .icon-placeholder, #manageSubjectIcon, .nav-item img{
-        width: 28px !important;
-        height: 28px !important;
+    
+    .nav-item {
+        padding: 5px; /* Reduced padding to fit smaller screens better */
+        flex: 1; /* Distributes space evenly across all 6 buttons */
     }
     
+    /* Make all icons uniformly larger */
+    .icon-placeholder, .nav-item img {
+        width: 32px !important;
+        height: 32px !important;
+        margin-bottom: 3px; /* Small gap between icon and text */
+    }
+    
+    /* Make text smaller and handle long text like "Add new subject" */
+    .nav-item span {
+        font-size: 10px !important;
+        line-height: 1.1; 
+        display: block;
+    }
+
+    /* Reset the desktop negative margin so it aligns with other text */
+    #manageSubjectBox span {
+        margin-top: 0;
+    }
 }
 
     </style>
 </head>
 <body>
     <aside class="sidebar">
-        <a href="index.php" class="nav-item"> 
-            <img src="Aset/logOutBtn.svg" alt="Log Out" class="icon-placeholder">
+        <a href="index.php" class="nav-item">
+            <img src="Aset/logOutBtn.svg" alt="Log Out" class="icon-placeholder" id="logOutIcon">
             <span>Log Out</span>
         </a>
 
         <a href="mainPage.php" class="nav-item">
-            <img src="Aset/homeBtn.svg" alt="Home" class="icon-placeholder">
+            <img src="Aset/homeBtn.svg" alt="Home" class="icon-placeholder" id="homeIcon">
             <span>Home</span>
         </a>
 
-        <a href="manageSubjectLecturer.php" class="nav-item" id="manageSubjectBox">
-            <img id="manageSubjectIcon" style="width: 50px; height: 50px;" src="Aset/manageSubjectBtn.svg" alt="Manage Subject" class="icon-placeholder">
-            <span>Manage Subject</span>
-        </a>
+<a href="manageSubjectLecturer.php" class="nav-item" id="manageSubjectBox">
+    <!-- Removed inline style="width: 50px; height: 50px;" -->
+    <img id="manageSubjectIcon" src="Aset/manageSubjectBtn.svg" alt="Manage Subject" class="icon-placeholder">
+    <span>Manage Subject</span>
+</a>
 
         <a href="reportLecturer.php" class="nav-item">
             <img src="Aset/reportBtn.svg" alt="Report" class="icon-placeholder">
             <span>Report</span>
         </a>
 
-        <a href="editQuizLecturer.php" class="nav-item">
-            <img style="width: 40px; height: 40px;" src="Aset/editQuizBtn.svg" alt="Edit Quiz" class="icon-placeholder">
-            <span>Edit Quiz</span>
-        </a>
+<a href="editQuizLecturer.php" class="nav-item">
+    <!-- Removed inline style="width: 40px; height: 40px;" -->
+    <img src="Aset/editQuizBtn.svg" alt="Edit Quiz" class="icon-placeholder">
+    <span>Edit Quiz</span>
+</a>
 
         <a href="addSubject.php" class="nav-item">
             <img src="Aset/addNewSubject.svg" alt="Add New Subject" class="icon-placeholder">
